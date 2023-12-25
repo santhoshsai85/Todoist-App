4797f79fe3306ebc1eb2ef1e4c23bdc4cd482913
Santhosh Sai Dhondi <santhoshsai85@gmail.com>
2023-12-25 21:44:34 +0000

    Todoist frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import { fetchAll } from './api/TaskService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const homeLoader = () => {
+  return fetchAll();
+}
+
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    loader: homeLoader,
+  },
+]);
+
+
+const App = () => {
+  return <RouterProvider router={browserRouter} />;
 }
 
 export default App;
